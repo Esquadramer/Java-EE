101dce30c62f4511155e14372a9cdd8e94231098
Sr Fearless <tiagofreitasmachado@hotmail.com>
2025-10-07 20:51:38 -0300

    Mandando para Github

---
+++ Aula/src/aula01/aula/Pessoa.java
@@ -0,0 +1,67 @@
+package aula01.aula;
+
+import java.util.Objects;
+
+// classe Pojo
+
+public class Pessoa {
+
+	private String nome;
+	private int idade;
+	
+	//construtor
+	
+	public Pessoa(String nome, int idade) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+	}
+	
+	public Pessoa() {
+		super();
+	}
+	
+	//getters e setters
+	//métodos de captura e configuração
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	//Sobrescrever os Métodos toStrings
+	
+	@Override
+	public String toString() {
+		return "Pessoa [nome=" + nome + ", idade=" + idade + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idade, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return idade == other.idade && Objects.equals(nome, other.nome);
+	}
+	
+}
